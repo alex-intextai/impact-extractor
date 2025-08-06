898747f0a17a344939fb4cb9a5f5d1c5d4ba3c1e
alex-intext.ai <alex@intextai.com>
2025-08-06 16:08:06 -0500

    Reader file

---
+++ pdf_reader/reader.py
@@ -0,0 +1,10 @@
+import pymupdf
+
+doc = pymupdf.open("../data/raw/Environmental Cost of AI _ FT.pdf")
+out = open("../data/outputs/out.txt", "wb") #create text output
+for page in doc: #iterate the doc pages
+    text = page.get_text().encode("utf-8") #get plain text
+    out.write(text)
+    out.write(bytes((12,))) 
+out.close()
+
