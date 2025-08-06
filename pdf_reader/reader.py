6a2108efe48bde454e6559494c09c31d8b29752e
alexkimrow <alexkimro@gmail.com>
2025-08-06 16:30:19 -0500

    reader

--- pdf_reader/reader.py
+++ pdf_reader/reader.py
@@ -1,6 +1,6 @@
 import pymupdf
 
-doc = pymupdf.open("../data/raw/Environmental Cost of AI _ FT.pdf")
+doc = pymupdf.open("../data/raw/Environmental Cost of AI _ FT.pdf") #open
 out = open("../data/outputs/out.txt", "wb") #create text output
 for page in doc: #iterate the doc pages
     text = page.get_text().encode("utf-8") #get plain text
