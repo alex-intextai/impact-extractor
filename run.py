bbe73f2f32244b17951ccf9f68ba57a65ad0f04e
alex-intextai <alex@intextai.com>
2025-08-08 16:35:15 -0500

    Extract, read and process to get sentence segmentations.

--- run.py
+++ run.py
@@ -1,2 +1,12 @@
-# CLI script for batch processing
+from pdf_reader.reader import extract_text, doc, out
+import sys, pathlib, pymupdf
+from nlp.sentence import process_text
+from nlp.model import read_text, classify_text
 
+def main():
+    extract_text(doc, out)
+    text = read_text("./data/outputs/out.txt")
+    process_text(text)
+
+if __name__ == "__main__":
+    main()
